88c2fbf5c73652508de7af9d0db165e50a1f9584
MK <99416359+MK316@users.noreply.github.com>
2025-02-24 18:36:16 +0900

    Create CEFR_Wordlist.py

---
+++ pages/CEFR_Wordlist.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import request
+
+
+# Create tabs for different sections of the course
+tabs = st.tabs(["🍐 Wordlist B1B2", "🍓 Wordlist C1"])
+
+# Content for the Schedule tab
+with tabs[0]:
+    st.caption("Spring 2025")
+    # URL of the raw markdown file on GitHub
+    markdown_url = "https://raw.githubusercontent.com/MK316/Engpro-Class/refs/heads/main/data/CEFRB1B2.txt"
+    
+    try:
+        response = requests.get(markdown_url)
+        response.raise_for_status()  # Raises an HTTPError for bad responses
+        markdown_content = response.text
+        st.markdown(markdown_content, unsafe_allow_html=True)
+    except requests.exceptions.HTTPError as err:
+        st.error(f"Failed to retrieve Markdown content: {err}")
+    except requests.exceptions.RequestException as e:
+        st.error(f"Request failed: {e}")
+
+
+with tabs[1]:
+    st.caption("Spring 2025")
+    # URL of the raw markdown file on GitHub
+    markdown_url = "https://raw.githubusercontent.com/MK316/Engpro-Class/refs/heads/main/data/CEFRC1.txt"
+    
+    try:
+        response = requests.get(markdown_url)
+        response.raise_for_status()  # Raises an HTTPError for bad responses
+        markdown_content = response.text
+        st.markdown(markdown_content, unsafe_allow_html=True)
+    except requests.exceptions.HTTPError as err:
+        st.error(f"Failed to retrieve Markdown content: {err}")
+    except requests.exceptions.RequestException as e:
+        st.error(f"Request failed: {e}")
