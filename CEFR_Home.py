6664e0fdab10665651cef9484eb3d9ffa97c897a
MK <99416359+MK316@users.noreply.github.com>
2025-02-25 11:07:08 +0900

    Update CEFR_Home.py

--- CEFR_Home.py
+++ CEFR_Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.markdown("""
-  ### Vocabulary practice application hub
+  ### ❄️ Vocabulary practice application hub
   This page introduces and provides guidance on using an app designed to help college freshmen and sophomores learn vocabulary with audio support. The vocabulary focuses on 2,000 additional words from the Oxford Learner’s Dictionary 3K, which have been incorporated to enhance learning. These additional 2K words correspond to CEFR B2 and C1 levels, supporting learners in expanding their academic and professional English skills.
   """)        
